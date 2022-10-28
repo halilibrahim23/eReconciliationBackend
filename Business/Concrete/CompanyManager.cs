6190ce478369e56ccf2748a77e81599194fe66af
H.Kaya <H.Kaya@DESKTOP-D88PV7P>
2022-10-29 01:01:56 +0300

    Add project files.

---
+++ Business/Concrete/CompanyManager.cs
@@ -0,0 +1,20 @@
+﻿using Business.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CompanyManager : ICompanyService
+    {
+        EfCompanyDal efCompanyDal = new EfCompanyDal();
+        public List<Company> GetList()
+        {
+            return efCompanyDal.GetList();
+        }
+    }
+}
